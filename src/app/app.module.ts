a5f67d000c90d5c6894b3fa4dc5b2210da071789
Nainiket <nainiketdesai98@gmail.com>
2020-06-09 13:18:17 -0400

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { CoreModule } from './@core/core.module';
 import { ThemeModule } from './@theme/theme.module';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+
 import {
   NbChatModule,
   NbDatepickerModule,
--- src/app/pages/dashboard/status-card/status-card.component.ts
+++ src/app/pages/dashboard/status-card/status-card.component.ts
@@ -1,10 +1,16 @@
 import { Component, Input } from '@angular/core';
+// import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
+// @Injectable()
+// export class ConfigService {
+  
+// }
 @Component({
   selector: 'ngx-status-card',
   styleUrls: ['./status-card.component.scss'],
   template: `
-    <nb-card (click)="on = !on" [ngClass]="{'off': !on}">
+    <nb-card (click)="on = !on; action()" [ngClass]="{'off': !on}">
       <div class="icon-container">
         <div class="icon status-{{ type }}">
           <ng-content></ng-content>
@@ -20,7 +26,18 @@ import { Component, Input } from '@angular/core';
 })
 export class StatusCardComponent {
 
+  constructor(private http: HttpClient) { }
+
+  // configUrl = 'https://cloud.hubitat.com/api/f9362e31-5634-4944-b222-25840fb45762/apps/2/devices/2/toggle?access_token=2c87cfb5-bc3f-4962-905a-01888fd90ac9';
+  action(){
+    
+    this.http.get<any>('https://cloud.hubitat.com/api/f9362e31-5634-4944-b222-25840fb45762/apps/2/devices/2/toggle?access_token=2c87cfb5-bc3f-4962-905a-01888fd90ac9').subscribe(
+      error => console.error('There was an error!', error)
+  );
+    }
+
   @Input() title: string;
   @Input() type: string;
   @Input() on = true;
 }
+
